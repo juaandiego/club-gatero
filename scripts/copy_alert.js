180d951c73d87a2bef75224c6926708d97a89b1a
Juan Diego Vasquez <juandvasquezd@gmail.com>
2022-12-17 10:14:17 -0300

    Snippet para copiar datos, ajustes al copy

---
+++ scripts/copy_alert.js
@@ -0,0 +1,51 @@
+const alertPlaceholder = document.getElementById('copyPlaceholder')
+
+const alert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible mb-0" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+}
+
+function CopyToClipboard(containerid) {
+    if (window.getSelection) {
+        if (window.getSelection().empty) {
+            window.getSelection().empty();
+        } else if (window.getSelection().removeAllRanges) {
+            window.getSelection().removeAllRanges();
+        }
+    } else if (document.selection) {
+        document.selection.empty();
+    }
+
+    if (document.selection) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(containerid));
+        range.select().createTextRange();
+        document.execCommand("copy");
+        document.selection.empty();
+    } else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(document.getElementById(containerid));
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        if (window.getSelection().empty) {
+            window.getSelection().empty();
+        } else if (window.getSelection().removeAllRanges) {
+            window.getSelection().removeAllRanges();
+        }
+    }
+}
+
+const alertTrigger = document.getElementById('copyAlertBtn')
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        CopyToClipboard('datosBanco');
+        alert('Datos bancarios copiados', 'success');
+    })
+}
